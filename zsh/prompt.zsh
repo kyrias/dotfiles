7a487f52fa2246d5186ceb7d58ee612aa676c515
Johannes Löthberg <johannes@kyriasis.com>
2014-10-15 18:42:58 +0200

    zsh/prompt: redid prompt, originally stolen from @HalosGhost

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,18 +1,31 @@
-DEFAULT_COLOR="%{${fg[default]}%}"
-function bold  { printf "%s%s%s" "%{%B%}" "$1" "%{%b%}" }
-function red   { printf "%s%s%s" "%{${fg[red]}%}" "$1" "$DEFAULT_COLOR" }
-function green { printf "%s%s%s" "%{${fg[green]}%}" "$1" "$DEFAULT_COLOR" }
-function blue  { printf "%s%s%s" "%{${fg[blue]}%}" "$1" "$DEFAULT_COLOR" }
-function cyan { printf "%s%s%s" "%{${fg[cyan]}%}" "$1" "$DEFAULT_COLOR" }
-function magenta { printf "%s%s%s" "%{${fg[magenta]}%}" "$1" "$DEFAULT_COLOR" }
-function yellow { printf "%s%s%s" "%{${fg[yellow]}%}" "$1" "$DEFAULT_COLOR" }
-function black { printf "%s%s%s" "%{${fg[black]}%}" "$1" "$DEFAULT_COLOR" }
-function white { printf "%s%s%s" "%{${fg[white]}%}" "$1" "$DEFAULT_COLOR" }
-function prompt_user_color {
-	(( UID )) && print -n red || print -n magenta
+function red
+	printf "%s%s%s" "%F{red}" "$@" "%f"
+function redbg
+	printf "%s%s%s" "%K{red}" "$@" "%k"
+function redbg_p
+	printf "%s%s%s" "%K{red}" "$@" "%k$(red $RSEGF$RSEG)"
+
+function bluebg_p
+	printf "%s%s%s" "%K{blue}" "$@" "%k%F{blue}$RSEGF$RSEG%f"
+
+function black
+	printf "%s%s%s" "%F{black}" "$@" "%f"
+function blackbg
+	printf "%s%s%s" "%K{black}" "$@" "%k"
+
+function white
+	printf "%s%s%s" "%F{white}" "$@" "%f"
+function whitebg
+	printf "%s%s%s" "%K{white}" "$@" "%k"
+
+function user_color {
+	(( UID )) && print -n black || print -n red
 }
 
-PROMPT="$(yellow "┌─[") $(yellow "$(bold %m)") \
-$($(prompt_user_color) "$(bold %n)") \
-$(blue "$(bold %~)") $(magenta $(bold '$branch'))$(yellow "]")
-$(yellow "└─╼") "
+RSEGF=""
+RSEG=""
+LSEGF=""
+LSEG=""
+BRNCH=""
+PROMPT='$($(user_color)bg " $(white %m) ")$(whitebg "$($(user_color) "$RSEGF %~") ")$RSEGF
+%(?.$(bluebg_p "%B ^_^ %b").$(redbg_p "%B o_O %b")) '

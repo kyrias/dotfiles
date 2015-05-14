9985d509e9ec86aab0dc813cc2ab371a75ae1293
Johannes Löthberg <johannes@kyriasis.com>
2015-05-14 13:36:10 +0200

    zsh: Add green prompt for nymeria

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,4 @@
+# Red
 bg[light_pink]='%K{001}'
 bg[light_red]='%K{196}'
 bg[red]='%K{88}'
@@ -9,6 +10,7 @@ fg[red]='%F{88}'
 fg[dark_red]='%F{52}'
 
 
+# Yellow
 bg[light_gold]='%K{136}'
 bg[gold]='%K{139}'
 bg[dark_gold]='%K{94}'
@@ -18,11 +20,16 @@ fg[gold]='%F{139}'
 fg[dark_gold]='%F{94}'
 
 
+# Blue
 bg[light_blue]='%K{blue}'
 bg[blue]='%K{21}'
 
 fg[light_blue]='%F{blue}'
 fg[blue]='%F{21}'
+
+
+# Green
+bg[dark_green]='%K{64}'
 
 function virtualenv_prompt {
 	if [[ -n "$VIRTUAL_ENV" ]]; then
@@ -43,6 +50,11 @@ $bg[light_blue]%B ^_^ %b %k'
 
 	"lucifer")
 		PROMPT='$bg[dark_gold] $fg[white]%m%f %k$bg[white]$fg[dark_gold] %~ %f%k$(virtualenv_prompt)
+$bg[light_blue]%B ^_^ %b %k'
+		;;
+
+	"nymeria")
+		PROMPT='$bg[dark_green] $fg[white]%m%f %k$bg[white]$fg[dark_gold] %~ %f%k$(virtualenv_prompt)
 $bg[light_blue]%B ^_^ %b %k'
 		;;
 

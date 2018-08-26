d3bfba9df85c6004910f1786335b2e07aa8b4c9d
Johannes Löthberg <johannes@kyriasis.com>
2018-08-26 20:46:41 +0200

    zsh: Remove old aliases
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -16,23 +16,10 @@ alias ls='\ls "${ls_options[@]}"'
 alias ll='\ls "${ls_options[@]}" -l'
 
 alias grep='\grep --color'
-
 alias mkdir='mkdir -vp'
 alias df='df -h'
-
-alias acp='acp -g'
-alias amv='amv -g'
-
-alias ncmpcpp='ncmpcpp -c ~/.config/ncmpcpp/config'
-alias wefree='weechat -a -r "/connect Freenode"'
-alias msa='mbsync --config "$XDG_CONFIG_HOME/mbsyncrc" -a'
-
 alias pt='pstree --highlight-all --long --uid-changes'
-
-alias sprin='curl -F "sprunge=<-" http://sprunge.us'
-sprfile() {
-	curl -F "sprunge=<$1" http://sprunge.us
-}
+alias pb='curl -F c=@- https://ptpb.pw/'
 
 # Colored man
 man() {
@@ -45,26 +32,6 @@ man() {
 	LESS_TERMCAP_us=$(printf "\e[1;32m") \
 	man "$@"
 }
-
-sp() { printf '%s' "$@"; printf '\n'; }
-
-have() { command -v "$1" >&/dev/null; }
-
-# service management
-if have systemctl && [[ -d /run/systemd/system ]]; then
-	alias ssctl='sudo systemctl'
-	alias userctl='systemctl --user'
-
-	alias list='systemctl list-units -t path,service,socket --no-legend'
-	alias ulist='userctl list-units -t path,service,socket --no-legend'
-
-	alias lcstatus='loginctl session-status $XDG_SESSION_ID'
-	alias tsd='tree /etc/systemd/system'
-
-	cgls() { SYSTEMD_PAGER='cat' systemd-cgls "$@"; }
-	usls() { cgls "/user.slice/user-$UID.slice/$*"; }
-	psls() { cgls "/user.slice/user-$UID.slice/session-$XDG_SESSION_ID.scope"; }
-fi
 
 tt() {
 	if [[ -n "$@" ]]; then

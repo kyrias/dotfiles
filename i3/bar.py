64c1b16e71ef4bbd4e6376b855234e0743a1185f
Johannes Löthberg <johannes@kyriasis.com>
2022-09-02 21:47:24 +0200

    i3: Update config
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- i3/bar.py
+++ i3/bar.py
@@ -12,12 +12,13 @@ import socket
 from i3pystatus import Status
 
 hostname = socket.gethostname()
-status = Status(standalone=True)
+status = Status(standalone=True, logfile='/home/kyrias/.cache/i3pystatus.log')
 
+font_feature_template='''<span font_features="zero, ss01, tnum">{}</span>'''
 
 status.register("clock",
                 color="#CDC0B0",
-                format="<span font_features=\"zero, ss01, tnum\">%Y-%m-%d %H:%M:%S%z</span>",
+                format=font_feature_template.format('%Y-%m-%d %H:%M:%S%z'),
                 hints={"markup": "pango"})
 
 
@@ -27,7 +28,7 @@ if hostname == "hydrogen.kyriasis.com":
                     full_color="#7CFC00",
                     charging_color="#7CFC00",
                     critical_color="#EE4000",
-                    format="⚡0 {percentage:.2f}% {remaining:%E%hh:%Mm}{status}",
+                    format=font_feature_template.format('⚡0 {percentage:.2f}% {remaining:%E%hh:%Mm}{status}'),
                     alert=True,
                     alert_percentage=5,
                     status={
@@ -35,14 +36,15 @@ if hostname == "hydrogen.kyriasis.com":
                         "CHR": "↑",
                         "FULL": "=",
                     },
-                    battery_ident="BAT0",)
+                    battery_ident="BAT0",
+                    hints={"markup": "pango"})
 
     status.register("battery",
                     color="#CDC0B0",
                     full_color="#7CFC00",
                     charging_color="#7CFC00",
                     critical_color="#EE4000",
-                    format="⚡1 {percentage:.2f}% {remaining:%E%hh:%Mm}{status}",
+                    format=font_feature_template.format('⚡1 {percentage:.2f}% {remaining:%E%hh:%Mm}{status}'),
                     alert=True,
                     alert_percentage=5,
                     status={
@@ -50,7 +52,8 @@ if hostname == "hydrogen.kyriasis.com":
                         "CHR": "↑",
                         "FULL": "=",
                     },
-                    battery_ident="BAT1",)
+                    battery_ident="BAT1",
+                    hints={"markup": "pango"})
 
 elif hostname.startswith("lithium"):
     status.register("battery",
@@ -102,7 +105,7 @@ else:
 
 status.register("temp",
                 color="#CDC0B0",
-                format="{Package_id_0}°C {Core_0_bar}{Core_1_bar}",
+                format=font_feature_template.format("{Package_id_0}°C {Core_0_bar}{Core_1_bar}"),
                 hints={"markup": "pango"},
                 lm_sensors_enabled=True)
 
@@ -110,13 +113,15 @@ status.register("temp",
 status.register("pulseaudio",
                 color_unmuted="#CDC0B0",
                 color_muted="#EE4000",
-                format="♪ {volume}%",)
+                format=font_feature_template.format("♪ {volume}%"),
+                hints={"markup": "pango"})
 
 
 status.register("backlight",
                 color="#CDC0B0",
                 backlight="intel_backlight",
-                format="🔆 {percentage}% ({brightness}/{max_brightness})")
+                format=font_feature_template.format("🔆 {percentage}%"),
+                hints={"markup": "pango"})
 
 
 if hostname == "zorg.kyriasis.com":
@@ -149,14 +154,18 @@ elif hostname == "hydrogen.kyriasis.com":
     status.register("network",
                     color_up="#7CFC00",
                     color_down="#EE4000",
-                    interface="wlp4s0",
-                    format_up="{essid:.10s}: {v4cidr} {quality:3.0f}%",)
+                    interface="wlan0",
+                    format_up=font_feature_template.format("{essid:.10s}: {v4cidr} {quality:3.0f}%"),
+                    format_down=font_feature_template.format("{interface}: DOWN"),
+                    hints={"markup": "pango"})
 
     status.register("network",
                     color_up="#7CFC00",
                     color_down="#EE4000",
                     interface="enp0s31f6",
-                    format_up="{interface}: {v4cidr}")
+                    format_up=font_feature_template.format("{interface}: {v4cidr}"),
+                    format_down=font_feature_template.format("{interface}: DOWN"),
+                    hints={"markup": "pango"})
 
 elif hostname.startswith('lithium'):
     status.register("network",
@@ -176,12 +185,14 @@ status.register("disk",
                 color="#CDC0B0",
                 path="/boot",
                 divisor=1024**2,
-                format="/boot {avail}M",)
+                format=font_feature_template.format("/boot {avail}M"),
+                hints={"markup": "pango"})
 
 status.register("disk",
                 color="#CDC0B0",
                 path="/",
-                format="/ {avail}G",)
+                format=font_feature_template.format("/ {avail}G"),
+                hints={"markup": "pango"})
 
 
 status.run()
